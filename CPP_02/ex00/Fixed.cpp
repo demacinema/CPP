6ed73378629068354bbf2a5fd06db3b38ad1571a
Demetrio Rodrigues <demrodri@student.42wolfsburg.de>
2024-12-10 18:16:40 +0100

    wip_fix_msg_ex00

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: demacinema <demacinema@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 22:32:57 by demacinema        #+#    #+#             */
-/*   Updated: 2024/11/20 22:33:02 by demacinema       ###   ########.fr       */
+/*   Updated: 2024/12/10 18:14:34 by demacinema       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,38 @@
 
 Fixed::Fixed() : _value(0)
 {
-	std::cout << " Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
-		std::cout << " Deconstructor called" << std::endl;
+		std::cout << "Destructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& other) : _value(other._value)
+Fixed::Fixed(const Fixed& other) //: _value(other.getRawBits()) //_value(other._value)
 {
-	std::cout << " Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
+	_value = other.getRawBits();
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-	std::cout << " Copy assignment operator called" << std::endl;
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
-		_value = other._value;
+	{
+		_value = other.getRawBits();
+	}
 	return *this;
 }
 
 int Fixed::getRawBits() const
 {
-	//std::cout << " getRawBits member function called" << std::endl;
+	std::cout << "getRawBits member function called" << std::endl;
 	return _value;
 }
 
 void Fixed::setRawBits(int value)
 {
-	//std::cout << " setRawBits member function called" << std::endl;
+	std::cout << "setRawBits member function called" << std::endl;
 	_value = value;
 }
--- CPP_02/ex00/main.cpp
+++ CPP_02/ex00/main.cpp
@@ -6,21 +6,23 @@
 /*   By: demacinema <demacinema@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 22:33:19 by demacinema        #+#    #+#             */
-/*   Updated: 2024/11/20 22:33:21 by demacinema       ###   ########.fr       */
+/*   Updated: 2024/12/10 18:13:39 by demacinema       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void)
+int main (void)
 {
-	Fixed	a;
-	Fixed	b(a);
-	Fixed	c;
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
 	c = b;
 
-	a.setRawBits(4);
-	b.setRawBits(2);
-	std::cout << a.getRawBits() << b.getRawBits() << c.getRawBits() << std::endl;
-	return (0);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
 }
